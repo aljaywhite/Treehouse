e68ee971af4771ffbcff88d4f406405575d81c98
Alan White <aljaywhite23@gmail.com>
2016-06-16 13:30:16 -0500

    Updated with jquery

--- AJAX Basics Widget/js/widget.js
+++ AJAX Basics Widget/js/widget.js
@@ -1,43 +1,34 @@
-//Employee Status
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
-      } else {
-        statusHTML += '<li class="out">';
-      }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
-    }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
-xhr.open('GET', '../data/employees.json');
-xhr.send();
+//Employees Status
 
-//Office Status
-var roomRequest = new XMLHttpRequest();
-roomRequest.onreadystatechange = function () {
-  if(roomRequest.readyState === 4 && roomRequest.status === 200) {
-    var rooms = JSON.parse(roomRequest.responseText);
-    var statusHTML = '<ul class="rooms">';
-    for (var i=0; i<rooms.length; i += 1) {
-      if (rooms[i].available === true) {
-        statusHTML += '<li class="empty">';
+$(document).ready(function () {
+  $.getJSON('../data/employees.json', function (data) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
       } else {
-        statusHTML += '<li class="full">';
+        statusHTML +='<li class="out">';
       }
-      statusHTML += rooms[i].room;
-      statusHTML += '</li>';
-    }
+      statusHTML += employee.name + '</li>';
+    });
     statusHTML += '</ul>';
-    document.getElementById('roomList').innerHTML = statusHTML;
-  }
-};
-roomRequest.open('GET', '../data/rooms.json');
-roomRequest.send();
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+  
+  //Office Status
+  
+  $.getJSON('../data/rooms.json', function (data) {
+    var statusHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        statusHTML +='<li class="empty">';
+      } else {
+        statusHTML +='<li class="full">';
+      }
+      statusHTML += room.room + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML)
+  }); // end getJSON
+}); // end ready
+
